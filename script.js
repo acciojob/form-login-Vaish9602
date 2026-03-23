d5c7505ad62c22f0ad66efce62eda3891e90c1d8
Vaish9602 <141707358+Vaish9602@users.noreply.github.com>
2026-03-23 15:50:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-function getFormvalue() {
-    //Write your code here
+ function getFormvalue(event) {
+      event.preventDefault(); // prevent page reload
 
-}
+      const fname = document.getElementsByName("fname")[0].value.trim();
+      const lname = document.getElementsByName("lname")[0].value.trim();
+
+      const fullName = fname + " " + lname;
+
+      alert(fullName);
